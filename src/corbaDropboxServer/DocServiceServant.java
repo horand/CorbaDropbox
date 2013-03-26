a2f259c6b5d8dbaa0a6f3f2400816e64be769d58
David Horan <david.horan@student.ncirl.ie>
2013-03-26 20:03:43 +0000

    Callback Method
    Callback Method added to IDL

--- src/corbaDropboxServer/DocServiceServant.java
+++ src/corbaDropboxServer/DocServiceServant.java
@@ -1,17 +1,11 @@
 package corbaDropboxServer;
 
+import CorbaDropbox.ClientCallback;
 import CorbaDropbox.DocServiceOperations;
 import CorbaDropbox.Document;
 import CorbaDropbox.User;
 
 public class DocServiceServant implements DocServiceOperations{
-
-	@Override
-	public boolean uploadDoc(String filename, String contents,
-			boolean isPrivate, User user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 	@Override
 	public Document downloadDoc(String filename, User user) {
@@ -29,6 +23,12 @@ public class DocServiceServant implements DocServiceOperations{
 	public String listDocuments() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean uploadDoc(Document document, ClientCallback updatedDoc) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
