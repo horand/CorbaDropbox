8ed00485419fd9d4aa30c2f3b63741f9d5018bc4
David Horan <david.horan@student.ncirl.ie>
2013-03-26 20:17:34 +0000

    IDL Resolved and Callback Method
    IDL confirmed as ok with structs for User and Document. Callback method
    in IDL.

---
+++ src/corbaDropboxClient/ClientCallbackServant.java
@@ -0,0 +1,13 @@
+package corbaDropboxClient;
+
+import CorbaDropbox.ClientCallbackOperations;
+
+public class ClientCallbackServant implements ClientCallbackOperations{
+
+	@Override
+	public void doCallback(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
